80062d0aed8d90429702b691e03da4b451dc640c
Sara Gunda <foodpiles@Saras-Air.fios-router.home>
2023-05-29 21:55:47 -0400

    begin routes

--- routes/index-routes.js
+++ routes/index-routes.js
@@ -1,13 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const siteRouter = require('./site-routes');
-const booksRouter = require('./book-routes');
-const adminRouter = require('./admin-routes');
+const siteRouter = require("./site-routes");
+const quizRouter = require("./quiz-routes");
+const adminRouter = require("./admin-routes");
 
-router.use('/', siteRouter);
-router.use('/books', booksRouter);
-router.use('/admin-console', adminRouter);
-
-
+router.use("/", siteRouter);
+router.use("/quiz", quizRouter);
+router.use("/admin-console", adminRouter);
 
 module.exports = router;
